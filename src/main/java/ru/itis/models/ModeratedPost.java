73570ae917388a9aa14e5a25e1969cc4bfbe0c82
Emil Davlyatov <emil.davlyatov2014@yandex.ru>
2022-03-21 22:00:13 +0500

    Minor changes

--- src/main/java/ru/itis/models/ModeratedPost.java
+++ src/main/java/ru/itis/models/ModeratedPost.java
@@ -12,6 +12,5 @@ public class ModeratedPost extends PostsBase {
             joinColumns = @JoinColumn(name = "post_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id")
     )
-    protected Set<Tag> tags;
-
+    private Set<Tag> tags;
 }
--- src/main/java/ru/itis/models/PostsBase.java
+++ src/main/java/ru/itis/models/PostsBase.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Set;
 import java.util.UUID;
 
 @MappedSuperclass
@@ -16,14 +15,14 @@ public abstract class PostsBase {
 
     @Id
     @Column(name = "id")
-    protected UUID id;
+    private UUID id;
 
     @Column(name = "post_text", columnDefinition = "text")
-    protected String postText;
+    private String postText;
 
     @Column(name = "book_name")
-    protected String bookName;
+    private String bookName;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    protected DefaultUser author;
+    private DefaultUser author;
 }
